f24d29ab9f0f26f7aca60bab55276d2d1f0c8ac6
蔡诗珺 <caisj@tingyun.com>
2018-07-30 20:48:15 +0800

    增加 SpringBoot2.0（八）集成通用 Mapper功能

---
+++ springboot2_universal_mapper_008/src/main/java/com/example/demo/service/UserInfoService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.core.universal.Service;
+import com.example.demo.model.UserInfo;
+
+public interface UserInfoService extends Service<UserInfo>{
+
+}
