a4d7f5687ce1f29a4bf49922f1dacc094d97505d
蔡诗珺 <caisj@tingyun.com>
2018-07-30 19:07:13 +0800

    增加 SpringBoot2.0（四）自定义消息转换器

---
+++ springboot2_custom_message_converters_004/src/main/java/com/example/demo/controller/UserInfoController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.core.ret.RetResponse;
+import com.example.demo.core.ret.RetResult;
+import com.example.demo.model.UserInfo;
+import com.example.demo.service.UserInfoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("userInfo")
+public class UserInfoController {
+
+    @Resource
+    private UserInfoService userInfoService;
+
+    @PostMapping("/hello")
+    public String hello(){
+        return "hello SpringBoot";
+    }
+
+    @PostMapping("/selectById")
+    public RetResult<UserInfo> selectById(Integer id){
+        UserInfo userInfo = userInfoService.selectById(id);
+        return RetResponse.makeOKRsp(userInfo);
+    }
+}
