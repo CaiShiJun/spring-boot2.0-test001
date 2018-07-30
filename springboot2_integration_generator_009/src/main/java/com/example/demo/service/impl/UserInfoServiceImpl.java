3d81acb86c2334be209d43500d3eeccbbc7c53d7
蔡诗珺 <caisj@tingyun.com>
2018-07-30 21:17:59 +0800

    增加 SpringBoot2.0（九）集成generator自动生成model，xml，dao功能

---
+++ springboot2_integration_generator_009/src/main/java/com/example/demo/service/impl/UserInfoServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.core.universal.AbstractService;
+import com.example.demo.core.ret.ServiceException;
+import com.example.demo.dao.UserInfoMapper;
+import com.example.demo.model.UserInfo;
+import com.example.demo.service.UserInfoService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserInfoServiceImpl extends AbstractService<UserInfo> implements UserInfoService{
+
+    @Resource
+    private UserInfoMapper userInfoMapper;
+
+    @Override
+    public UserInfo selectById(String id){
+        UserInfo userInfo = userInfoMapper.selectByPrimaryKey(id);
+        if(userInfo == null){
+            throw new ServiceException("暂无该用户");
+        }
+        return userInfo;
+    }
+}
