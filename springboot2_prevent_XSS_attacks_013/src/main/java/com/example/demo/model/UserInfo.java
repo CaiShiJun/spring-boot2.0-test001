c4d425ffa77fa2a1e69c3d08a470410626742946
蔡诗珺 <caisj@tingyun.com>
2018-07-31 13:34:22 +0800

    增加 SpringBoot2.0（十三）添加防止XSS攻击功能

---
+++ springboot2_prevent_XSS_attacks_013/src/main/java/com/example/demo/model/UserInfo.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class UserInfo {
+
+    /**
+     * 主键
+     */
+    @Id
+    private String id;
+
+    /**
+     * 用户名
+     */
+    @Column(name = "user_name")
+    private String userName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
